3efe67f0ef98faadc78057591a5f1a9ccc0afb9c
Akshay Gulabrao <aksgula22@gmail.com>
2026-05-13 10:10:33 -0700

    update

--- build.sh
+++ build.sh
@@ -1,34 +1,13 @@
 #!/bin/sh
-# for f in *.md; do
-#   pandoc "$f" -o "${f%.md}.html" \
-#         --mathjax --css=style.css -s
-# done
-
 pandoc index.md -o "index.html" --mathjax --css=style.css -s
-pandoc abvx.md -o abvx.html --mathjax --css=style.css -s --citeproc
-pandoc eq.md -o eq.html --mathjax --css=style.css -s --citeproc
 pandoc supply-chains.md -o supply-chains.html --mathjax --css=style.css -s --citeproc
-pandoc capr.md -o capr.html --mathjax --css=style.css -s --citeproc
-pandoc sprb.md -o sprb.html --mathjax --css=style.css -s --citeproc
-pandoc avxl.md -o avxl.html --mathjax --css=style.css -s --citeproc
 pandoc lottery_EV.md -o lottery_EV.html --mathjax --css=style.css -s --citeproc
-pandoc biotech_week_of_nov2.md -o biotech_week_of_nov2.html --mathjax --css=style.css -s --citeproc
-
-
 
 git add index.md index.html
-git add abvx.md abvx.html
-git add avxl.md avxl.html
-git add eq.md eq.html
-git add capr.md capr.html
-git add sprb.md sprb.html
 git add supply-chains.md supply-chains.html
 git add lottery_EV.md lottery_EV.html
-git add biotech_week_of_nov2.md biotech_week_of_nov2.html
-
 
 git add build.sh
-git add media/*
 
 git commit -m "update"
 git push
--- index.md
+++ index.md
@@ -7,16 +7,8 @@ This website is built with markdown files and a pandoc build script. Most static
 
 ---
 
-## Biotech Price Targets
-- [AVXL: AF710B](./avxl.html)
-- [CAPR: CAP-1002](./capr.html)
-- [SPRB ($400 ~mid 2027): TA-ERT](./sprb.html)
-- [ABVX ($200 ~mid 2027): ABX464](./abvx.html)
-- [EQ ($20 ~mid 2030): EQ504](./eq.html)
-
 ## Notes
 - [Supply chains that run the world](./supply-chains.html)
 
 ## Blog
-- [Biotech Week of Nov 2](./biotech_week_of_nov2.html)
 - [Expected Value of a Lottery Ticket in CA](./lottery_EV.html)
